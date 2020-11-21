28bdbf2618e79fbd2c708d21446cd8adc55262e0
HungLe051902 <43202901+HungLe051902@users.noreply.github.com>
2020-11-21 16:49:15 +0700

    Thêm một số thuộc tính cho class Part7

---
+++ ToeicExamOnline/Repositories/Entities/Part7.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToeicExamOnline.Repositories.Entities
+{
+    public class Part7
+    {
+        public Guid GroupQuestionID { get; set; }
+        public int QuestionNo { get; set; }
+        public int ExamNo { get; set; }
+    }
+}
