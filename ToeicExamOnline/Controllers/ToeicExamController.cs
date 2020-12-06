7aafb2ab59d2b4d565872d5ba5edf5fcdad70dc4
HungLe051902 <43202901+HungLe051902@users.noreply.github.com>
2020-12-06 11:54:17 +0700

    Yêu cầu token khi gọi các service toeicexam

--- ToeicExamOnline/Controllers/ToeicExamController.cs
+++ ToeicExamOnline/Controllers/ToeicExamController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ToeicExamOnline.Repositories.Entities;
 using ToeicExamOnline.Services.Interfaces;
 
 namespace ToeicExamOnline.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ToeicExamController : ControllerBase
